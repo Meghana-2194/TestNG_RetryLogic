612f23384d1130d27aa1d7e95d19a96af59af11c
M <Meghana.M@unisys.com>
2020-06-15 18:07:58 +0530

    RetryLogic Concepts

---
+++ src/test/java/MyTest/TestLevel.java
@@ -0,0 +1,26 @@
+package MyTest;
+
+import org.testng.annotations.Test;
+
+import junit.framework.Assert;
+
+public class TestLevel {
+
+	@Test
+
+	public void Test1() {
+		Assert.assertEquals(false, true);
+	}
+
+	@Test
+
+	public void Test2() {
+		Assert.assertEquals(false, true);
+	}
+	
+	@Test
+	public void Test3() {
+		Assert.assertEquals(false, true);
+	}
+
+}
